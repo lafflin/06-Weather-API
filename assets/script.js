3085e18ee712ba9167f19443d9989e0ad0bca224
Max McLaughlin <maxwelljmack@gmail.com>
2022-07-21 18:16:48 -0700

    init

---
+++ assets/script.js
@@ -0,0 +1,42 @@
+// DOM elements
+const bodyEl = $("body");
+const mainEl = $("#main");
+const searchEl = $("#search");
+const mainWeatherEl = $("#mainWeather");
+const fiveDayEl = $("#fiveDay");
+
+// api stuff
+const apiKey = "d586f9f8d444d305ec48c97bc8680507";
+let city = "";
+
+// elements for the search bar and submit buttons
+const citySearchEl = $(
+	"<input type='text' id='searchInput'> <input type='submit' value='Get Weather' id='searchButton'>"
+);
+searchEl.append(citySearchEl);
+
+$("#searchButton").click(function () {
+	console.log($("#searchInput"));
+	city = $("#searchInput").val();
+	console.log(city);
+	getWeather();
+});
+// there should exist a search history for past searches as well
+// after hitting enter, a main box for the weather info should pop up below the search bar (including city name, date, weather icon, temp, humidity, wind speed, uv index)
+// when viewing the uv index there should be a color indicator of the condition; favorable(green), moderate(yellow), or severe(red)
+//below these there should be a panel of the five day forecast. inside of each days card there should be date, an icon for weather,  temperature, wind speed, and humidity
+
+// more api stuff
+var queryURL =
+	"https://api.openweathermap.org/data/2.5/weather?q=" +
+	city +
+	"&appid=" +
+	apiKey;
+function getWeather() {
+	fetch(queryURL)
+		.then((res) => res.json())
+		.then((data) => {
+			let weather = data;
+			console.log(weather);
+		});
+}
