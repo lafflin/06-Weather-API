4d556d32cf6c26674abae76bcf4d095263f4f8f4
Max McLaughlin <maxwelljmack@gmail.com>
2022-07-25 17:23:34 -0700

    global variable storage issue currently

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ const fiveDayEl = $("#fiveDay");
 
 // api stuff
 const apiKey = "d586f9f8d444d305ec48c97bc8680507";
-let city = "";
+var city = "";
 
 // elements for the search bar and submit buttons
 const citySearchEl = $(
@@ -15,17 +15,16 @@ const citySearchEl = $(
 );
 searchEl.append(citySearchEl);
 
+// inside of this function need to eventually store each individual successful search into local storage to be displayed for search history
 // this needs to be fixed, currently works as far as setting the city to the entered city, but won't add it to the API call
 $("#searchButton").click(function () {
 	console.log($("#searchInput"));
-	city = $("#searchInput").val();
+	currentCity = $("#searchInput").val();
+	console.log(currentCity);
+	city = JSON.stringify(currentCity);
 	console.log(city);
 	getWeather();
 });
-// there should exist a search history for past searches as well
-// after hitting enter, a main box for the weather info should pop up below the search bar (including city name, date, weather icon, temp, humidity, wind speed, uv index)
-// when viewing the uv index there should be a color indicator of the condition; favorable(green), moderate(yellow), or severe(red)
-//below these there should be a panel of the five day forecast. inside of each days card there should be date, an icon for weather,  temperature, wind speed, and humidity
 
 // more api stuff
 var queryURL =
@@ -41,3 +40,7 @@ function getWeather() {
 			console.log(weather);
 		});
 }
+
+// after hitting enter, a main box for the weather info should pop up below the search bar (including city name, date, weather icon, temp, humidity, wind speed, uv index)
+// when viewing the uv index there should be a color indicator of the condition; favorable(green), moderate(yellow), or severe(red)
+//below these there should be a panel of the five day forecast. inside of each days card there should be date, an icon for weather,  temperature, wind speed, and humidity
